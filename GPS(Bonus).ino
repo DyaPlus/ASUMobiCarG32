127aeeedd587272e4a6490bb67d8a940fd892089
Helaly96 <30905312+Helaly96@users.noreply.github.com>
2018-07-08 03:17:37 +0200

    Added GPS code for the Bonus level

---
+++ GPS(Bonus).ino
@@ -0,0 +1,22 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial gps (0,1);
+
+char data  ;
+
+void  setup ()
+{
+ Serial . begin (115200);            
+ gps. begin (9600);
+}
+
+
+void  loop ()
+{
+  if (gps. available ())
+  {
+    data = gps. read ();
+    Serial . print (data);
+  }
+}
+
